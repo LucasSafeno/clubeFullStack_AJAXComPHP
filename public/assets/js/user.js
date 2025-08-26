c3ce982759e224f474499b293e4991fc49fbc37c
Lucas Safeno <lugnorio@gmail.com>
2025-08-26 20:06:37 -0300

    Ajax com PHP#09 - Usando a estrutura ajax refatorada

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -1,35 +1,48 @@
+var btn_users = document.querySelector('#btn-users');
+var div_users = document.querySelector('#div-users');
+
 window.onload = function () {
-  var xhttp = new XMLHttpRequest();
-
-  var btn_users = document.querySelector('#btn-users');
-  var div_users = document.querySelector('#div-users');
-
   btn_users.onclick = function () {
+    // A função xmlHttpGet inicia a requisição AJAX.
+    // O segundo argumento é a função de callback que será executada
+    // sempre que o estado da requisição (readyState) mudar.
+    xmlHttpGet('ajax/user', function () {
+      // Esta função interna agora é o seu onreadystatechange.
+      // Ela será chamada múltiplas vezes durante o ciclo de vida da requisição.
 
+      // Mostra o ícone de "loading" enquanto a requisição não estiver completa.
+      beforeSend(function () {
+        div_users.innerHTML = `<i class="fa fa-refresh fa-spin fa-3x fa-fw"></i><span class="sr-only"> Loading</span>`;
+      });
 
-    xhttp.onreadystatechange = function () {
-      if (xhttp.readyState == 4 && xhttp.status == 200) {
-        var users = JSON.parse(this.responseText);
+      // Quando a requisição for bem-sucedida (readyState 4 e status 200),
+      // o conteúdo da div é atualizado com a tabela.
+      success(function () {
+        var users = JSON.parse(xhttp.responseText);
+        var table = `<table class="table table-striped">`;
 
-        var table = `<table class='table table-striped'>`;
-        table += `<thead><tr><td>ID</td><td>Nome</td><td>E-mail</td></tr><thead>`;
+        table += `<thead><tr><td>ID</td><td>Name</td><td>Email</td></tr></thead>`;
+
+        // Note que aqui faltava um ">" no seu código original
         table += `<tbody>`;
+
         users.forEach(function (user) {
           table += `<tr>`;
-
           table += `<td>${user.id}</td>`;
           table += `<td>${user.name}</td>`;
           table += `<td>${user.email}</td>`;
-
-          table += `</td>`;
+          table += `</tr>`;
         });
-        table += `</tbody>`;
-        table += `</table>`;
+
+        table += `</tbody></table>`;
 
         div_users.innerHTML = table;
-      }
-    };
-    xhttp.open('GET', 'ajax/user.php', true);
-    xhttp.send();
+      });
+
+      // Trata o erro, caso ocorra.
+      error(function () {
+        div_users.innerHTML = 'Ocorreu um erro';
+      });
+    });
   };
 };
